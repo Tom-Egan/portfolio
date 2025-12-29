64c927975ebd0f071543adb540f460f795035386
Tom Egan <tom.eeegan@gmail.com>
2025-12-29 09:10:55 -0800

    Case study checkpoint
    Cleaned up copy
    Added alt tags to images
    Converted some divs to <section>

--- js/main.js
+++ js/main.js
@@ -60,14 +60,21 @@ function updateYearsOfService() {
 // ==========================
 
 async function loadIncludes() {
-  const includes = document.querySelectorAll('[data-include]');
+  const includes = document.querySelectorAll("[data-include]");
 
   for (const el of includes) {
-    const file = el.dataset.include;
-    const res = await fetch(file);
-    el.innerHTML = await res.text();
+    const file = el.getAttribute("data-include");
+
+    try {
+      const res = await fetch(file);
+      if (!res.ok) throw new Error(`Failed to load ${file}`);
+      el.innerHTML = await res.text();
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
+
 
 // ==========================
 // Site Initialization
